28a5ce58b01625e031afcfd553888771509b0d46
Raul <rchfe23@gmail.com>
2023-01-16 15:58:55 -0500

    Update lib.py

--- convert_to_txt/lib.py
+++ convert_to_txt/lib.py
@@ -1,7 +1,7 @@
 """Library that has useful functions for building other ebook management tools.
 
-This is a Python port of `convert-to-txt.sh` and `lib.sh`_ from `ebook-tools`_ written in shell by
-`na--`_.
+This is a Python port of `convert-to-txt.sh` and `lib.sh`_ from `ebook-tools` written in shell by
+`na--`.
 
 Ref.: 
 - https://github.com/na--/ebook-tools/blob/master/convert-to-txt.sh
